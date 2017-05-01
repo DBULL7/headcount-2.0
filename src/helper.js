e86dbf4944f7b630ad7d585771533a4825f35da1
Devon Bull <dbull@live.com>
2017-05-01 16:07:31 -0600

    First test passing, need to reduce objects

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,24 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = data
+  }
+  cleanData() {
+    let nonDuplicatedData = this.data.reduce((acc, dataPoint) => {
+      dataPoint.forEach( => {
+        if acc {
+          console.log('duplicate!')
+        } else {
+          acc.push(key)
+        }
+      })
+      return acc
+    }, [] )
+  }
+
+  // test() {
+  // let test = this.data.filter(dataPoint =>
+  //   !dataPoint['Data'])
+  // console.log(test)
+  // }
 
 }
