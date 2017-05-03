59691230c5c10ee25571cc86c1a5419857a0d1a8
Devon Bull <dbull@live.com>
2017-05-02 23:46:42 -0600

    Cards now have data displayed correctly

--- src/Card.js
+++ src/Card.js
@@ -4,23 +4,27 @@ class Card extends Component {
   constructor() {
     super()
     this.state = {
-      years: '',
-      yearData: ''
+      // id: Date.now()
     }
   }
 
   displayData() {
     let test = Object.keys(this.props.data)
     let test2 = Object.values(this.props.data)
-
+    let test3 = test.map((year, index) => {
+      return (
+        <h5 key={index}>{year} : {test2[index]}</h5>
+      )
+    })
+    return test3
     // console.log(Object.values(test))
   }
 
   render() {
-    {this.displayData()}
     return (
       <div className='card'>
         {this.props.title}
+        {this.displayData()}
       </div>
     )
   }
