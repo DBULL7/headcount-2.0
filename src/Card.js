343dbcefa3cf2e6fc8cdec175378bb799859b5ee
Devon Bull <dbull@live.com>
2017-05-02 23:08:08 -0600

    Data is now passing to cards correctly

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 
 class Card extends Component {
+  constructor() {
+    super()
+    this.state = {
+      years: '',
+      yearData: ''
+    }
+  }
+
+  displayData() {
+    let test = Object.keys(this.props.data)
+    let test2 = Object.values(this.props.data)
+
+    // console.log(Object.values(test))
+  }
+
   render() {
+    {this.displayData()}
     return (
       <div className='card'>
         {this.props.title}
--- src/CardGrid.js
+++ src/CardGrid.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Card from './Card'
 
+
 const CardGrid = ({data}) => {
+  let test = Object.values(data)
+  let test2 = []
+  test.forEach(obj => {
+    // console.log(obj.data)
+    test2.push(obj.data)
+  })
   return (
     <div>
       {
-        Object.keys(data).map(district => {
-          return <Card title={district} />
+        Object.keys(data).map((district, index) => {
+
+          return <Card
+                  title={district}
+                  data={test2[index]}
+                  key={index}
+                 />
         })
       }
     </div>
